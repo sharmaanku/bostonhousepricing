ea4eb2605189ced2e650d1602268a9ab34aa8d3e
ankur sharma <s.ank@hotmial.co.in>
2022-12-06 10:42:19 +0530

    Chnaged the dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,9 @@ EXPOSE $PORT
 CMD gunicorn --workers=4 --bind 0.0.0.0:$PORT app:app
 
 
+
+
+
+
+
+
---
+++ MLTest.py
@@ -0,0 +1,6 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, Activation, MaxPooling2D, Dense, Flatten
+from keras.preprocessing.image import ImageDataGenerator
+
+
